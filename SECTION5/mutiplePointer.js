a20bde3d7ce9bfe44949113cd9786cc83db9f738
y-minion <dudals2963@tukorea.ac.kr>
2025-08-29 11:56:07 +0900

    다중 포인터 코드 리팩토링 완료

--- SECTION5/mutiplePointer.js
+++ SECTION5/mutiplePointer.js
@@ -12,4 +12,42 @@ function countUniqueValues(arr) {
   return console.log(Object.keys(counterObj).length);
 }
 
-countUniqueValues([1, 1, 1, 1, 1, 2]);
+// countUniqueValues([1, 1, 1, 1, 1, 2]);
+
+function betterCountUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+
+  //1. 포인터 두개를 생성한다.
+  let pointer1 = 0;
+  let pointer2 = 0;
+  //2. 두개의 포인터는 0 인덱스부터 시작하여 오른쪽으로 전진한다.
+  //5. 포인터 2가 가르키는 인덱스가 배열의 길이보다 클 경우 종료
+  while (pointer2 < arr.length) {
+    //3. 포인터 1과 2가 가르키는 값이 같을 경우 포인터 2를 전진한다.
+    if (arr[pointer1] === arr[pointer2]) {
+      pointer2++;
+    }
+    //4-1. 포인터 1과 2가 가르키는 값이 다를경우에 포인터 1을 한칸 오른쪽으로 이동시킨다.
+    if (arr[pointer1] !== arr[pointer2]) {
+      pointer1++;
+      //4-2. 이동시킨 공간에 포인터 2의 값을 삽입한다.
+      arr[pointer1] = arr[pointer2];
+      pointer2++;
+    }
+  }
+  //6. 포인터 1이 가르키는 인덱스의 위치+1을 반환한다.
+  console.log(pointer1 + 1);
+}
+betterCountUniqueValues([1]);
+
+function countUniqueValues2(arr) {
+  if (arr.length === 0) return 0;
+  let pointer = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[pointer] !== arr[i]) {
+      pointer++;
+      arr[pointer] = arr[i];
+    }
+  }
+  return pointer + 1;
+}
